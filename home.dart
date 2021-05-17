969a30eb4bd3ce695a71534e4c90c80381ed7e3f
Santhosh2141 <64309210+Santhosh2141@users.noreply.github.com>
2021-05-17 18:27:22 +0530

    Add files via upload

---
+++ home.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:patient_assistant/screens/Survey.dart';
+import '../services/auth_service.dart';
+import 'Survey.dart';
+
+class HomePage extends StatefulWidget {
+  //const HomePage({Key key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                text: 'SURVEY',
+
+              ),
+              Tab(
+                text: 'SCHEDULE',
+              ),
+              Tab(
+                text: 'CHATBOT',
+              ),
+            ],
+          ),
+          leading: GestureDetector(
+            onTap: () {
+              showModalBottomSheet(context: context, builder: (context){
+                return Container(
+                  padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text("TEXT"),
+                    ],
+                  ),
+                );
+              }
+              );
+            },
+            child: Icon(
+              Icons.person,  // add custom icons also
+            ),
+          ),
+          title: Text('HOME PAGE',),
+          centerTitle: true,
+          actions: <Widget>[
+
+            IconButton(
+              icon: Icon(Icons.logout),
+              onPressed: () async
+              {
+                await _auth.signOut();
+              },
+            ),
+
+
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            Stack(
+              children: [
+                Survey()
+              ],
+            ),
+            Stack(
+              children: [
+                Container(
+                  constraints: BoxConstraints.expand(),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/5026563.jpg'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+            Stack(
+              children: [
+                Container(
+                  constraints: BoxConstraints.expand(),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/5026563.jpg'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
